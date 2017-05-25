b61a4b747e8a2543798d30c792ed004b6add6d1b
Vince207 <vincecs207@gmail.com>
2017-05-25 10:59:59 -0700

    Spinner fixed, EditText added

--- app/src/main/java/com/example/vince/pana7/AddFragment.java
+++ app/src/main/java/com/example/vince/pana7/AddFragment.java
@@ -4,18 +4,14 @@ package com.example.vince.pana7;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.EditText;
 
 public class AddFragment extends DialogFragment
 {
+    private EditText spinnerText;
 
     public Dialog onCreateDialog(Bundle bundle)
     {
@@ -23,45 +19,19 @@ public class AddFragment extends DialogFragment
         View additionDialogView = getActivity().getLayoutInflater().inflate(R.layout.fragment_add, null);
         builder.setView(additionDialogView);
         builder.setTitle(R.string.menuitem_add);
+
+        spinnerText = (EditText) additionDialogView.findViewById(R.id.spinner_edit);
+        builder.setCancelable(false).setPositiveButton("Ok", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                spinnerText.setText(spinnerText.getText());
+            }
+        });
+
         return builder.create();
     }
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-
-        View view = inflater.inflate(R.layout.fragment_add, container, false);
-        return view;
-
-
-    }
-
-    public void onViewCreated(View rootView, Bundle savedInstanceState)
-    {
-        super.onViewCreated(rootView, savedInstanceState);
-        final View v = rootView;
-
-        String groceries = getString(R.string.groceries_name);
-        String utilities = getString(R.string.utilities_name);
-        String transportation = getString(R.string.transportation_name);
-        String eatingout = getString(R.string.eating_out_name);
-        String entertainment = getString(R.string.entertainment_name);
-
-        Spinner spinner = (Spinner) rootView.findViewById(R.id.add_spinner);
-
-        List<String> selections = new ArrayList<String>();
-
-        selections.add(groceries);
-        selections.add(utilities);
-        selections.add(transportation);
-        selections.add(eatingout);
-        selections.add(entertainment);
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, selections);
-
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(dataAdapter);
-
-    }
 
 }
